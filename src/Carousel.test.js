22a90f3718cde98272ce043a0939b1dd02080be9
Eric Huie <erichuie97@gmail.com>
2024-01-16 17:18:18 -0800

    added some tests for first and last image arrows

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -50,7 +50,11 @@ it("works when you click on the left arrow", function() {
   ).not.toBeInTheDocument();
 
   //move backward in the carousel
-  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  //space between elements in query selector looks for descendant of a selector
+  //.bi bi-arrow-left-circle looks for descendant of bi that
+  // is bi-arrow-left-circle
+  //we want look for both classes individually of .bi and .bi-arrow-left-circle
+  const leftArrow = container.querySelector(".bi.bi-arrow-left-circle");
   debug(leftArrow)
   fireEvent.click(leftArrow);
 
@@ -61,6 +65,71 @@ it("works when you click on the left arrow", function() {
   expect(
     container.querySelector('img[alt="testing image 1"]')
   ).toBeInTheDocument();
+});
+
+it("left arrow missing on first image", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // expect the first image to show
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+
+  // expect right arrow visible
+  expect(
+    container.querySelector('.bi-arrow-right-circle')
+  ).toBeInTheDocument();
+
+  // expect left arrow to not be visible
+  expect(
+    container.querySelector('.bi-arrow-left-circle')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  // const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  // fireEvent.click(rightArrow);
+
+  // // expect the second image to show, but not the first
+  // expect(
+  //   container.querySelector('img[alt="testing image 1"]')
+  // ).not.toBeInTheDocument();
+  // expect(
+  //   container.querySelector('img[alt="testing image 2"]')
+  // ).toBeInTheDocument();
+});
+
+it("right arrow missing on last image", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect the last image to show
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).toBeInTheDocument();
+
+  // expect right arrow to not be visible
+  expect(
+    container.querySelector('.bi-arrow-right-circle')
+  ).not.toBeInTheDocument();
+
+  // expect left arrow to be visible
+  expect(
+    container.querySelector('.bi-arrow-left-circle')
+  ).not.toBeInTheDocument();
 });
 
 //smoke test
